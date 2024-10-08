73563e3a685bbb759b71bfc244d56b46a04bd5af
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-10-08 09:10:30 -0700

    Update twrp_LH8n.mk

--- twrp_LH8n.mk
+++ twrp_LH8n.mk
@@ -15,9 +15,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 #Inherit emulated_storage properties
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Configure virtual_ab_ota.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
 # Configure launch_with_vendor_ramdisk.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
