cb78f101d4028c53ea38cf57f978a10ed3159937
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-11-03 03:29:56 -0800

    i dunno
    add this line for i dunno

--- twrp_LH8n.mk
+++ twrp_LH8n.mk
@@ -12,11 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-#Inherit emulated_storage properties
+# Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Configure launch_with_vendor_ramdisk.mk
+# Enable Virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
