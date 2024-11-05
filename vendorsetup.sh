3d65d15e0b2b7fc5fb5d610707a4e8ad0fbea210
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-11-05 01:50:13 -0800

    fix

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ RET=0
 REVERSE=0
 
 cd bootable/recovery
-git apply --reverse --check ../../device/infinix/X6739/patches/0001-Change-haptics-activation-file-path.patch || REVERSE=$?
+git apply --reverse --check ../../device/tecno/LH8n/patches/0001-Change-haptics-activation-file-path.patch || REVERSE=$?
 cd ../../
 
 if [ -f "$TFILE" ];then
@@ -13,7 +13,7 @@ elif [ $REVERSE -eq 0 ]; then
 	echo "$TFILE is not found but git is able to reverse haptics path patch, assuming it's already patched, skipping"
 else
     cd bootable/recovery
-    git apply ../../device/infinix/X6739/patches/0001-Change-haptics-activation-file-path.patch || RET=$?
+    git apply ../../device/tecno/LH8n/patches/0001-Change-haptics-activation-file-path.patch || RET=$?
     cd ../../
     if [ $RET -ne 0 ];then
 	echo "ERROR: minuitwrp/events.cpp could not be patched! Vibration in TWRP will not work."
