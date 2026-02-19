97f07abfdaf57e0b5ed7395eee74a0007eb02e0b
nazephyrus <126683488+naden01@users.noreply.github.com>
2026-02-20 02:22:27 +0700

    LH8n : fix decryption on A15+

--- device.mk
+++ device.mk
@@ -21,6 +21,7 @@ AB_OTA_PARTITIONS += \
     product \
     system \
     system_ext \
+	vbmeta \
     vbmeta_system \
     vbmeta_vendor \
     vendor \
@@ -35,57 +36,67 @@ PRODUCT_PACKAGES += \
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
-    FILESYSTEM_TYPE_system=erofs \
+    FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=erofs \
+    FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-service \
     android.hardware.boot@1.2-mtkimpl \
     android.hardware.boot@1.2-mtkimpl.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
-    bootctrl
+    bootctl
 
+# Update engine
 PRODUCT_PACKAGES += \
-    bootctrl.mt6833 \
-    bootctrl.mt6833.recovery
+    update_engine \
+    update_engine_sideload \
+    update_verifier
 
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+	
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
-
-# Drm
+	
+# Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4
-
+    android.hardware.keymaster@4.1
+	
+# Security
+PRODUCT_PACKAGES += \
+    android.hardware.security.keymint \
+    android.hardware.security.secureclock \
+    android.hardware.security.sharedsecret
+	
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1
-
-# Additional Libraries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    android.hardware.keymaster@4.1 \
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1
-
-# Keystore2
+# Keystore Hal
 PRODUCT_PACKAGES += \
     android.system.keystore2
 
+# Additional configs
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1
+
+TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hardware.keymaster@4.1
+	
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
